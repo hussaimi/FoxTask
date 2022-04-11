34af88832b62380e57d3569d5a17178edd1383fb
Muhammad Moiz Hussain <mohammadmoiz6@gmail.com>
2022-04-11 13:15:23 -0400

    Example Instrumented Test Fixed

--- app/src/androidTest/java/com/example/foxTask/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/foxTask/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.foxtask", appContext.packageName)
+        assertEquals("com.example.foxTask", appContext.packageName)
     }
 }
